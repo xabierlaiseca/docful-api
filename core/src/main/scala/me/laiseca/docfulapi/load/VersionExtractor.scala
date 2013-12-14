6bd084906ef53684ebbff313715b232cfce0e337
xabierlaiseca <xabier@laiseca.me>
2013-12-14 12:47:16 +0000

    Versions since and until are no longer optional

--- core/src/main/scala/me/laiseca/docfulapi/load/VersionExtractor.scala
+++ core/src/main/scala/me/laiseca/docfulapi/load/VersionExtractor.scala
@@ -3,11 +3,16 @@ package me.laiseca.docfulapi.load
 import me.laiseca.docfulapi.model.Version
 
 trait VersionExtractor {
+  protected def firstVersion:String
+  protected def lastVersion:String
+  
   def extractVersion(versionMap: Option[Map[String, String]]):Version = {
     extractVersion(versionMap.getOrElse(Map[String, String]()))
   }
   
   def extractVersion(versionMap: Map[String, String]) = {
-    new Version(versionMap.get("since"), versionMap.get("deprecated"), versionMap.get("until"))
+    new Version(versionMap.getOrElse("since", firstVersion), 
+        versionMap.get("deprecated"), versionMap.getOrElse("until", lastVersion))
   }
+  
 }
--- core/src/test/scala/me/laiseca/docfulapi/load/VersionExtractorSpec.scala
+++ core/src/test/scala/me/laiseca/docfulapi/load/VersionExtractorSpec.scala
@@ -2,35 +2,39 @@ package me.laiseca.docfulapi.load
 
 import org.scalatest.FlatSpec
 import me.laiseca.docfulapi.model.Version
+import me.laiseca.docfulapi.model.Versionable
 
 class VersionExtractorSpec extends FlatSpec {
+  private val firstVersion = "0.0.1"
+  private val lastVersion = "0.0.5"
   
   "given a version map with entries for all the values" should "create a complete Version instance" in {
     val versionMap = Map[String, String]("since" -> "0.0.1",
         "deprecated" -> "0.0.2", "until" -> "0.0.3")
-    val testObj = new Object with VersionExtractor
+    val testObj = new VersionExtractorObject(firstVersion, lastVersion)
         
-    assertResult(new Version(Option.apply("0.0.1"), 
-        Option.apply("0.0.2"), Option.apply("0.0.3"))){
+    assertResult(new Version("0.0.1", Option.apply("0.0.2"), "0.0.3")) {
       testObj.extractVersion(versionMap)
     }
   }
   
-  "given a version map with entries for none of the values" should "create an empty Version instance" in {
+  "given a version map with entries for none of the values" should "create a Version instance with required fields" in {
     val versionMap = Map[String, String]()
-    val testObj = new Object with VersionExtractor
+    val testObj = new VersionExtractorObject(firstVersion, lastVersion)
         
-    assertResult(new Version(Option.empty, Option.empty, Option.empty)){
+    assertResult(new Version(firstVersion, Option.empty, lastVersion)) {
       testObj.extractVersion(versionMap)
     }
   }
   
-  "given an empty option map with entries for none of the values" should "create an empty Version instance" in {
+  "given an empty option map with entries for none of the values" should "create a Version instance with required fields" in {
     val versionMap = Option.empty[Map[String, String]]
-    val testObj = new Object with VersionExtractor
+    val testObj = new VersionExtractorObject(firstVersion, lastVersion)
         
-    assertResult(new Version(Option.empty, Option.empty, Option.empty)){
+    assertResult(new Version(firstVersion, Option.empty, lastVersion)) {
       testObj.extractVersion(versionMap)
     }
   }
 }
+
+case class VersionExtractorObject(protected val firstVersion:String, protected val lastVersion:String) extends VersionExtractor
